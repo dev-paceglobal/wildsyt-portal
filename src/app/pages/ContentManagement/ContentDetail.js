8e0ba1b8f1fb0b573669ef80efd626df887a0cb2
Syed Danial <daniald3200@gmail.com>
2023-09-26 05:42:56 +0500

    working on content managment

--- src/app/pages/ContentManagement/ContentDetail.js
+++ src/app/pages/ContentManagement/ContentDetail.js
@@ -2,33 +2,32 @@ import {Grid, Paper} from '@mui/material'
 import React, {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
 import useContentById from '../../../hooks/useContentById'
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import './style.css'
 
 const ContentDetail = () => {
   const {id} = useParams()
 
   const [getContent, content] = useContentById()
 
-  const [value, setValue] = useState('');
-
-  console.log('id', id)
+  const [value, setValue] = useState('')
 
   useEffect(() => {
     getContent(id)
   }, [])
 
   console.log('content', content)
+  console.log('value', value)
 
   return (
     <>
       <Grid container justifyContent='center'>
         <Grid item xs={8}>
           <Paper elevation={3} sx={{padding: '30px'}}>
-          <ReactQuill theme="snow" value={value} onChange={setValue} />
+            <ReactQuill theme='snow' value={value} onChange={setValue} />
 
-            <div dangerouslySetInnerHTML={{__html: content.content}} />
+            <div className='contentDiv' dangerouslySetInnerHTML={{__html: content.content}} />
           </Paper>
         </Grid>
       </Grid>
